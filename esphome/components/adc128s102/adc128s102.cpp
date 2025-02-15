2423b04c423b35125d9ee62821ee2193ccf797b6
Greg <gkasprow@gmail.com>
2025-02-15 12:23:00 +0100

    added adc128s102 second transfer to correctly switch mux

--- esphome/components/adc128s102/adc128s102.cpp
+++ esphome/components/adc128s102/adc128s102.cpp
@@ -22,7 +22,10 @@ uint16_t ADC128S102::read_data(uint8_t channel) {
   uint8_t adc_primary_byte = this->transfer_byte(control);
   uint8_t adc_secondary_byte = this->transfer_byte(0x00);
   this->disable();
-
+  this->enable();
+  uint8_t adc_primary_byte = this->transfer_byte(control);
+  uint8_t adc_secondary_byte = this->transfer_byte(0x00);
+  this->disable();
   uint16_t digital_value = adc_primary_byte << 8 | adc_secondary_byte;
 
   return digital_value;
